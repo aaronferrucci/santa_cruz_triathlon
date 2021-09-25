d9a9e1da4dc3201f6cacdb20d1fcbbf6def4e451
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2021-09-25 13:49:16 -0700

    data cleaning and processing

--- main.R
+++ main.R
@@ -1,25 +1,35 @@
-
+library(ggplot2)
 library(lubridate)
+
 to_seconds <- function(t) {
   s <- seconds(hms(t))
   return(s)
+}
+
+process <- function(data) {
+  # convert times to absolute seconds
+  time_names <- c("Start", "Swim", "T1", "Bike", "T2", "Run", "USAT.Penalty", "Elapsed")
+  data[time_names] <- sapply(data[time_names], to_seconds)
+
+  # Elapsed=0 for some teams; I assume that means non-completion. Drop those entries
+  data <- data[data$Elapsed > 0,]
+
+  # each value is the end of its interval
+  # there's probably some 'clever' way to do these telescoping sums
+  data$Swim <- data$Swim + data$Start
+  data$T1 <- data$T1 + data$Swim
+  data$Bike <- data$Bike + data$T1
+  data$T2 <- data$T2 + data$Bike
+  data$Run <- data$Run + data$T2
+  data$End <- data$Run + data$USAT.Penalty
+  return(data)
 }
 
 read_data <- function(f) {
   data <- read.csv(f, sep='\t', stringsAsFactors = T)
   data$Division <- as.factor(data$Division)
 
-  # convert times to absolute seconds
-  time_names <- c("Start", "Swim", "T1", "Bike", "T2", "Run", "USAT.Penalty", "Elapsed")
-  data[time_names] <-sapply(data[time_names], to_seconds)
-
-  # Elapsed=0 for some teams; I assume that means non-completion. Drop those entries
-  data <- data[data$Elapsed > 0,]
-
-  # What to do with non-zero USAT.Penalty?
-
-  # each value except for "Start" is the end of its interval
   return(data)
 }
 
-relay_2019 <- read_data("relay_2019.csv")
+relay_2019 <- process(read_data("relay_2019.csv"))
