bcfec6512261af93b4f80243695ba42cca771692
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2021-09-25 10:39:59 -0700

    progress on cleaning the data

--- main.R
+++ main.R
@@ -1,5 +1,25 @@
 
-# read relay data
-relay2019 <- read.csv("relay_2019.csv", sep='\t', stringsAsFactors = T)
-# may want to covert Division into a factor
+library(lubridate)
+to_seconds <- function(t) {
+  s <- seconds(hms(t))
+  return(s)
+}
 
+read_data <- function(f) {
+  data <- read.csv(f, sep='\t', stringsAsFactors = T)
+  data$Division <- as.factor(data$Division)
+
+  # convert times to absolute seconds
+  time_names <- c("Start", "Swim", "T1", "Bike", "T2", "Run", "USAT.Penalty", "Elapsed")
+  data[time_names] <-sapply(data[time_names], to_seconds)
+
+  # Elapsed=0 for some teams; I assume that means non-completion. Drop those entries
+  data <- data[data$Elapsed > 0,]
+
+  # What to do with non-zero USAT.Penalty?
+
+  # each value except for "Start" is the end of its interval
+  return(data)
+}
+
+relay_2019 <- read_data("relay_2019.csv")
