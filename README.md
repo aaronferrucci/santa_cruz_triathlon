425f8c8946d5a121afc756e5a1f040ac336686b2
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2021-09-26 15:49:51 -0700

    2021 relay-mixed results as stacked barplot

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # santa_cruz_triathlon
 Exploring the santa cruz triathlon data set. 
-Here are the relay team results from 2019:
+Here are the mixed relay team results from 2021:
 
-![alt text](https://github.com/aaronferrucci/santa_cruz_triathlon/blob/main/2019.png "PNG plot")
+![alt text](https://github.com/aaronferrucci/santa_cruz_triathlon/blob/main/relay_2021_mixed.png "PNG plot")
--- main.R
+++ main.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+library(reshape)
 library(lubridate)
 
 to_seconds <- function(t) {
@@ -7,59 +8,64 @@ to_seconds <- function(t) {
 }
 
 process <- function(data) {
-  # convert times to absolute seconds
-  time_names <- c("Start", "Swim", "T1", "Bike", "T2", "Run", "USAT.Penalty", "Elapsed")
+  # start time - not interesting?
+  data <- subset(data, select = -c(Start.Time, Chip.Elapsed))
+
+  time_names <- c("Swim", "T1", "Bike", "T2", "Run")
+
   for (name in time_names) {
     data[name] <- to_seconds(data[[name]])
   }
 
-  # Elapsed=0 for some teams; I assume that means non-completion. Drop those entries
-  data <- data[data$Elapsed > 0,]
-
-  # each value is the end of its interval
-  # there's probably some 'clever' way to do these telescoping sums
-  data$Swim <- data$Swim + data$Start
-  data$T1 <- data$T1 + data$Swim
-  data$Bike <- data$Bike + data$T1
-  data$T2 <- data$T2 + data$Bike
-  data$Run <- data$Run + data$T2
-  data$End <- data$Run + data$USAT.Penalty
-
-  # distances are swim: 1.5km, bike: 40km, run: 10km
-  data$Start.d <- 0
-  data$Swim.d <- 1
-  data$T1.d <- data$Swim.d
-  data$Bike.d <- data$T1.d + 1
-  data$T2.d <- data$Bike.d
-  data$Run.d <- data$T2.d + 1
+  # Some data shows 0 for some of the segments; no-show or untidy data? Omit.
+  data <- data[data$Swim > 0 & data$Bike > 0 & data$Run > 0,]
 
   return(data)
 }
 
 read_data <- function(f) {
   data <- read.csv(f, sep='\t', stringsAsFactors = F)
-  data$Division <- as.factor(data$Division)
+  data$Division <- factor(data$Division)
+  data$Team.Name <- factor(data$Team.Name)
+  # order the team name according to division place, so the bar plot comes out
+  # in ascending order, left to right. Feels like an unseemly blending of data
+  # and presentation, but it's easy.
+  data$Team.Name <- reorder(data$Team.Name, data$Div.Place)
 
   return(data)
 }
 
 time_fmt <- function(x) {
   hour <- x %/% 3600
-  minute <- (xbreaks %% 3600) %/% 60
-  second <- xbreaks %% 60
+  minute <- (x %% 3600) %/% 60
+  second <- x %% 60
   return(sprintf("%d:%02d", hour, minute))
 }
 
-relay_2019 <- process(read_data("relay_2019.csv"))
-xbreaks <- seq(to_seconds("8:30:00"), to_seconds("13:30:00"), to_seconds("0:30:00"))
-p1 <- ggplot(relay_2019, aes(color=Team)) +
-  scale_y_continuous(breaks=0.5 + seq(0, 2), labels=c("Swim", "Bike", "Run"), limits=c(0, NA)) +
-  theme(axis.text.y = element_text(angle = 90, hjust=0.5)) +
-  scale_x_continuous(breaks=xbreaks, labels=time_fmt(xbreaks)) +
-  xlab("Time") + ylab("Segment") +
-  facet_grid(Division ~ ., as.table = FALSE) +
-  geom_segment(aes(x=Start, y=Start.d, xend=Swim, yend=Swim.d)) +
-  geom_segment(aes(x=Swim, y=Swim.d, xend=T1, yend=T1.d), color="gray") +
-  geom_segment(aes(x=T1, y=T1.d, xend=Bike, yend=Bike.d)) +
-  geom_segment(aes(x=Bike, y=Bike.d, xend=T2, yend=T2.d), color="gray") +
-  geom_segment(aes(x=T2, y=T2.d, xend=Run, yend=Run.d))
+raw <- process(read_data("relay_2021_mixed.csv"))
+# reswizzle the data for stacked barplot
+relay_2021 <- melt(raw, id=c("Bib", "Team.Name", "City", "Division", "Div.Place"))
+
+# reorder the phases so that they're stacked up in time
+relay_2021$variable <- factor(relay_2021$variable, rev(c("Swim", "T1", "Bike", "T2", "Run")))
+
+ybreaks <- seq(to_seconds("0:00:00"), to_seconds("4:00:00"), to_seconds("0:30:00"))
+
+p <- ggplot(relay_2021, aes(fill=variable, y=value, x=Team.Name)) +
+  theme(axis.text.x = element_text(angle = 90, hjust=0.5)) +
+  xlab("Team") + ylab("Time (H:MM)") +
+  scale_y_continuous(breaks=ybreaks, labels=time_fmt(ybreaks)) +
+  geom_bar(position="stack", stat="identity")
+
+# xbreaks <- seq(to_seconds("8:30:00"), to_seconds("13:30:00"), to_seconds("0:30:00"))
+# p1 <- ggplot(relay_2019, aes(color=Team)) +
+#   scale_y_continuous(breaks=0.5 + seq(0, 2), labels=c("Swim", "Bike", "Run"), limits=c(0, NA)) +
+#   theme(axis.text.y = element_text(angle = 90, hjust=0.5)) +
+#   scale_x_continuous(breaks=xbreaks, labels=time_fmt(xbreaks)) +
+#   xlab("Time") + ylab("Segment") +
+#   facet_grid(Division ~ ., as.table = FALSE) +
+#   geom_segment(aes(x=Start, y=Start.d, xend=Swim, yend=Swim.d)) +
+#   geom_segment(aes(x=Swim, y=Swim.d, xend=T1, yend=T1.d), color="gray") +
+#   geom_segment(aes(x=T1, y=T1.d, xend=Bike, yend=Bike.d)) +
+#   geom_segment(aes(x=Bike, y=Bike.d, xend=T2, yend=T2.d), color="gray") +
+#   geom_segment(aes(x=T2, y=T2.d, xend=Run, yend=Run.d))
